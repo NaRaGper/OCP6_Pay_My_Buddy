759e63797527c919a71dd98f30c16fd86fda9acd
NaRaGper <81639056+NaRaGper@users.noreply.github.com>
2022-03-26 05:37:01 +0100

    Ajout des dossiers "images" et "scripts"

---
+++ scripts/Insertion des valeurs de test.sql
@@ -0,0 +1,5 @@
+-- population de la table `Users` :
+INSERT INTO `Users` (`email`, `password`) VALUES ('test1@mail.fr', 'password1234');
+INSERT INTO `Users` (`email`, `password`, `username`) VALUES ('test2@mail.fr', '12password34', 'User2');
+INSERT INTO `Users` (`email`, `password`, `balance`, `bank_account_number`) VALUES ('test.3@mail.fr', '333password', 10000, '0138297465BIC');
+INSERT INTO `Users` (`email`, `password`, `username`, `balance`, `bank_account_number`) VALUES ('test44@mail.fr', 'p4ssword44', 'User4', 29283098, '02473655757AAA');
